ae0602bad9164f917aa19c233317714a65b4e65f
fomidtk <workout.nakamori@outlook.jp>
2021-05-23 18:10:09 +0900

    カウンター

---
+++ works/counter.js
@@ -0,0 +1,10 @@
+let count = 0
+const countUp = () => {
+    count++
+    const countElement = document.querySelector('#count')
+    countElement.innerText = '回数: ' + count
+  }
+const countreset = () => {
+    const countElement = document.querySelector('#count')
+    countElement.innerText = '回数: ' + 0
+  }
